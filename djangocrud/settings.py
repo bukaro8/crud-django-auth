2057493b57a6908a9249d6624b343bc595f661fb
Victor Ramirez <vicdeveloper83@gmail.com>
2025-07-02 19:07:17 +0100

    migrations made

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-roakgng*py%&%a_nd$5q=85(i-&8sh0$ony$x+bx-)f&)6x!s@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
